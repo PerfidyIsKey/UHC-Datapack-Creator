3cf3d02d75159ae7ab3d0f93746483db50cbd8a1
PerfidyIsKey <myday.games@gmail.com>
2020-09-08 12:52:32 +0200

    First commit

---
+++ src/ControlPoint.java
@@ -0,0 +1,68 @@
+public class ControlPoint {
+
+    private String name;
+
+    private int maxVal;
+    private int addRate;
+
+    private int x;
+    private int y;
+    private int z;
+
+    public ControlPoint(String name, int maxVal, int addRate, int x, int y, int z) {
+        this.name = name;
+        this.maxVal = maxVal;
+        this.addRate = addRate;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxVal() {
+        return maxVal;
+    }
+
+    public void setMaxVal(int maxVal) {
+        this.maxVal = maxVal;
+    }
+
+    public int getAddRate() {
+        return addRate;
+    }
+
+    public void setAddRate(int addRate) {
+        this.addRate = addRate;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+}
---
+++ src/Recipe.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class Recipe {
+    String type;
+    String[] grid;
+    ArrayList<String> keys;
+    String resultItem;
+    int resultAmount;
+
+    public Recipe(String type, String[] grid, ArrayList<String> keys, String resultItem, int resultAmount) {
+        this.type = type;
+        this.grid = grid;
+        this.keys = keys;
+        this.resultItem = resultItem;
+        this.resultAmount = resultAmount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(String[] grid) {
+        this.grid = grid;
+    }
+
+    public ArrayList<String> getKeys() {
+        return keys;
+    }
+
+    public void setKeys(ArrayList<String> keys) {
+        this.keys = keys;
+    }
+
+    public String getResultItem() {
+        return resultItem;
+    }
+
+    public void setResultItem(String resultItem) {
+        this.resultItem = resultItem;
+    }
+
+    public int getResultAmount() {
+        return resultAmount;
+    }
+
+    public void setResultAmount(int resultAmount) {
+        this.resultAmount = resultAmount;
+    }
+}
