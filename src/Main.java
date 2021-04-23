5db171abf0b5de4aca89229f559f4aa47e386422
Snodog627 <bthemen4@gmail.com>
2021-04-23 20:46:24 +0200

    Updated coordinates for UHC S40

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
     //GameData<
     private static int chestSize = 27;
     private static String admin = "PerfidyIsKey";
-    private static String startCoordinates = "-3 72 5";
+    private static String startCoordinates = "0 66 -4";
     private ArrayList<Team> teams = new ArrayList<>();
     private ArrayList<ControlPoint> controlPoints = new ArrayList<>();
     private ArrayList<CarePackage> carePackages = new ArrayList<>();
@@ -171,8 +171,8 @@ public class Main {
             teams.add(team);
         }
 
-        ControlPoint cp1 = new ControlPoint("CP1", 48000, 2, 112, 63, 90);
-        ControlPoint cp2 = new ControlPoint("CP2", 48000, 3, -23, 63, -158);
+        ControlPoint cp1 = new ControlPoint("CP1", 48000, 2, -122, 64, 77);
+        ControlPoint cp2 = new ControlPoint("CP2", 48000, 3, 38, 63, 180);
         controlPoints.add(cp1);
         controlPoints.add(cp2);
 
@@ -183,7 +183,7 @@ public class Main {
 
         CarePackage carePackage2 = new CarePackage("anti_cp", "Anti Controlpoint Drop",
                 "[{Slot:1b,id:\"minecraft:gunpowder\",Count:1b},{Slot:2b,id:\"minecraft:gunpowder\",Count:1b},{Slot:3b,id:\"minecraft:tnt\",Count:1b},{Slot:4b,id:\"minecraft:flint_and_steel\",Count:1b},{Slot:5b,id:\"minecraft:tnt\",Count:1b},{Slot:6b,id:\"minecraft:sand\",Count:1b},{Slot:7b,id:\"minecraft:sand\",Count:1b},{Slot:11b,id:\"minecraft:enchanted_book\",Count:1b,tag:{StoredEnchantments:[{lvl:4s,id:\"minecraft:blast_protection\"}]}},{Slot:12b,id:\"minecraft:lava_bucket\",Count:1b},{Slot:13b,id:\"minecraft:tnt\",Count:1b},{Slot:14b,id:\"minecraft:lava_bucket\",Count:1b},{Slot:15b,id:\"minecraft:enchanted_book\",Count:1b,tag:{StoredEnchantments:[{lvl:4s,id:\"minecraft:blast_protection\"}]}},{Slot:19b,id:\"minecraft:sand\",Count:1b},{Slot:20b,id:\"minecraft:sand\",Count:1b},{Slot:21b,id:\"minecraft:tnt\",Count:1b},{Slot:22b,id:\"minecraft:flint_and_steel\",Count:1b},{Slot:23b,id:\"minecraft:tnt\",Count:1b},{Slot:24b,id:\"minecraft:gunpowder\",Count:1b},{Slot:25b,id:\"minecraft:gunpowder\",Count:1b}]",
-                -158, 64, -23);
+                -116, 63, 141);
         carePackages.add(carePackage2);
 
         scoreboardObjectives.add(new ScoreboardObjective("Admin", "dummy"));
