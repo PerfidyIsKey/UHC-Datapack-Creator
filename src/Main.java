d52a36bf55af7f3a9e1b68d5c9f2e327d89c938c
Wouter Baltus <w.baltus@resoftware.nl>
2022-03-23 12:54:35 +0100

    Made a seperate class to handle most file operations

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,9 @@ public class Main {
         new Main().run();
     }
 
+    //packages
+    FileTools fileTools;
+
     //DatapackData<
     private static int gameMode = 1;
     /*
@@ -64,13 +67,17 @@ public class Main {
 
 
     private void run() {
+
         boolean menuRunning = true;
         Scanner scanner = new Scanner(System.in);
         String input;
         initSaveDir();
+
+        fileTools = new FileTools(version, dataPackLocation, dataPackName, worldLocation);
+
         initGameData();
         makeFunctionFiles();
-        makeRecipeFiles();
+        files.addAll(fileTools.makeRecipeFiles());
         makeLootTableFiles();
         System.out.println("Files available:\n");
         for (FileData data : files) {
@@ -86,12 +93,12 @@ public class Main {
             } else if (input.startsWith("update ")) {
                 String command = input.replace("update ", "");
                 if (command.equals("all")) {
-                    updateAllFiles();
+                    fileTools.updateAllFiles(files, fileLocation);
                 } else {
                     for (FileData f : files) {
                         if (f.getNameWithoutExtension().equals(command)) {
                             try {
-                                writeFile(f, fileLocation);
+                                fileTools.writeFile(f, fileLocation);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -100,7 +107,7 @@ public class Main {
                 }
             } else if (input.equals("create datapack")) {
                 try {
-                    createDatapack();
+                    fileTools.createDatapack(files, fileLocation);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -108,104 +115,6 @@ public class Main {
                 System.out.println("Input not recognized.");
             }
         }
-    }
-
-    private void updateAllFiles() {
-        for (FileData f : files) {
-            try {
-                writeFile(f, fileLocation);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void createDatapack() throws IOException {
-        File file = new File(dataPackLocation + dataPackName);
-        if (file.mkdir()) {
-            File pack = new File(dataPackLocation + dataPackName + "\\pack.mcmeta");
-            BufferedWriter writer = new BufferedWriter(new FileWriter(pack));
-            writer.write(" {");
-            writer.newLine();
-            writer.write("       \"pack\": {");
-            writer.newLine();
-            writer.write("           \"pack_format\": 3,");
-            writer.newLine();
-            writer.write("           \"description\": \"A datapack designed specifically for UHC. Version: " + version + ".\"");
-            writer.newLine();
-            writer.write("       }");
-            writer.newLine();
-            writer.write("   }");
-            writer.close();
-
-            File data = new File(dataPackLocation + dataPackName + "\\data");
-            if (data.mkdir()) {
-                File uhc = new File(dataPackLocation + dataPackName + "\\data\\uhc");
-                if (uhc.mkdir()) {
-                    File functions = new File(dataPackLocation + dataPackName + "\\data\\uhc\\functions");
-                    if (!functions.mkdir()) {
-                        System.out.println("Something went wrong creating Datapack 1");
-                    }
-                    File recipes = new File(dataPackLocation + dataPackName + "\\data\\uhc\\recipes");
-                    if (!recipes.mkdir()) {
-                        System.out.println("Something went wrong creating Datapack 2");
-                    }
-                    File lootTables = new File(dataPackLocation + dataPackName + "\\data\\uhc\\loot_tables");
-                    if (lootTables.mkdir()) {
-                        updateAllFiles();
-                    } else {
-                        System.out.println("Something went wrong creating Datapack 3");
-                    }
-                }
-            }
-            String from = "generated";
-            String to = worldLocation + "\\generated";
-            copyDirectory(from, to);
-        } else {
-            System.out.println("Datapack already exists: Updating files now...");
-            updateAllFiles();
-        }
-    }
-
-    public static void copyDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation)
-            throws IOException {
-        Files.walk(Paths.get(sourceDirectoryLocation))
-                .forEach(source -> {
-                    Path destination = Paths.get(destinationDirectoryLocation, source.toString()
-                            .substring(sourceDirectoryLocation.length()));
-                    try {
-                        Files.copy(source, destination);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-    }
-
-    private void writeFile(FileData fileData, String fileLocation) throws IOException {
-        ArrayList<String> fileText = fileData.getFileText();
-        File file;
-        if (fileData.getType().equals("recipe")) {
-            file = new File(fileLocation + "recipes\\" + fileData.getName() + ".json");
-        } else if (fileData.getType().equals("loot_tables")) {
-            file = new File(fileLocation + "loot_tables\\" + fileData.getName() + ".json");
-        }
-        else {
-            file = new File(fileLocation + "functions\\" + fileData.getName() + ".mcfunction");
-        }
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        for (String s : fileText) {
-            writer.write(s);
-            writer.newLine();
-        }
-        if (fileData.getType().equals("function")) {
-            writer.write("#This file was made using the \"UHC-Datapack-Creator\" current version: " + version + ".");
-            writer.newLine();
-            writer.write("#If you want to make any changes to this file please contact the UHC-Committee member: Perfidy.");
-            writer.newLine();
-            writer.write("#He will know how to change it. (Without messing things up...)");
-        }
-        writer.close();
-        System.out.println("File \"" + fileData.getName() + "\" Updated.");
     }
 
     private void initSaveDir() {
@@ -314,7 +223,7 @@ public class Main {
                 players.add(new Player("Vermeil_Chan",0));
                 players.add(new Player("Jobbo2002",7));
 
-                quotes = GetLinesFromFile("Files\\Diorite\\quotes.txt");
+                quotes = fileTools.GetLinesFromFile("Files\\Diorite\\quotes.txt");
 
 
                 break;
@@ -369,7 +278,7 @@ public class Main {
                 players.add(new Player("Jobbo2002",88));
 
                 // Quotes
-                quotes = GetLinesFromFile("Files\\URE\\quotes.txt");
+                quotes = fileTools.GetLinesFromFile("Files\\URE\\quotes.txt");
 
                 break;
         }
@@ -418,23 +327,6 @@ public class Main {
         effect.add(new StatusEffect("speed",20,1));
     }
 
-    private ArrayList<String> GetLinesFromFile(String pathname) {
-        ArrayList<String> fileLines = new ArrayList<>();
-        try {
-            File myObj = new File(pathname);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                fileLines.add(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-        return fileLines;
-    }
-
     private void makeLootTableFiles() {
         ArrayList<LootTableEntry> lootEntry = new ArrayList<>();
 
@@ -479,125 +371,10 @@ public class Main {
         lootEntry.add(new LootTableEntry(18,"amethyst_block",new LootTableFunction(16)));
         lootEntry.add(new LootTableEntry(15,"copper_block",new LootTableFunction(16)));
 
-        ArrayList<String> fileCommands = generateLootTable(lootEntry);
+        ArrayList<String> fileCommands = fileTools.generateLootTable(lootEntry);
 
         FileData fileData = new FileData("supply_drop", fileCommands, "loot_tables");
         files.add(fileData);
-    }
-
-    private ArrayList<String> generateLootTable(ArrayList<LootTableEntry> lootEntry) {
-        ArrayList<String> fileCommands = new ArrayList<String>();
-                fileCommands.add("{");
-        fileCommands.add("  \"type\": \"minecraft:chest\",");
-        fileCommands.add("  \"pools\": [");
-        fileCommands.add("    {");
-        fileCommands.add("      \"rolls\": {");
-        fileCommands.add("        \"min\": 3,");
-        fileCommands.add("        \"max\": 5");
-        fileCommands.add("      },");
-        fileCommands.add("      \"bonus_rolls\": 5,");
-        fileCommands.add("      \"entries\": [");
-
-        int counter = 1;
-        int totalWeight = 0;
-        for (LootTableEntry l : lootEntry)
-        {
-            fileCommands.add("        {");
-            fileCommands.add("          \"type\": \"minecraft:item\",");
-            fileCommands.add("          \"weight\": " + l.getWeight() + ",");
-            if (l.getFunction() != null) {
-                fileCommands.add("          \"name\": \"minecraft:" + l.getName() + "\",");
-                fileCommands.add("          \"functions\": [");
-                fileCommands.add("            {");
-                fileCommands.add("              \"function\": \"minecraft:set_count\",");
-                if (l.getFunction().getChance() > 0) {
-                    fileCommands.add("              \"count\": " + l.getFunction().getCount() + ",");
-                    fileCommands.add("              \"conditions\": [");
-                    fileCommands.add("                {");
-                    fileCommands.add("                  \"condition\": \"minecraft:random_chance\",");
-                    fileCommands.add("                  \"chance\": " + l.getFunction().getChance());
-                    fileCommands.add("                }");
-                    fileCommands.add("              ]");
-                }
-                else
-                {
-                    fileCommands.add("              \"count\": " + l.getFunction().getCount());
-                }
-                fileCommands.add("            }");
-                fileCommands.add("          ]");
-            }
-            else
-            {
-                fileCommands.add("          \"name\": \"minecraft:" + l.getName() + "\"");
-            }
-            if (counter < lootEntry.size()) {
-                fileCommands.add("        },");
-                counter++;
-            }
-            else {
-                fileCommands.add("        }");
-            }
-            totalWeight += l.getWeight();
-        }
-
-        fileCommands.add("      ]");
-        fileCommands.add("    }");
-        fileCommands.add("  ]");
-        fileCommands.add("}");
-        //fileCommands.add("#Total weight = "+ totalWeight);
-        return fileCommands;
-    }
-
-    private void makeRecipeFiles() {
-        ArrayList<Recipe> recipes = new ArrayList<>();
-
-        String[] grid = {" ", " ", " ", "1", "2", "1", " ", " ", " "};
-        ArrayList<String> keys = new ArrayList<>();
-        keys.add("ender_eye");
-        keys.add("black_wool");
-        Recipe recipe = new Recipe("crafting_shaped", grid, keys, "dragon_head", 1);
-        recipes.add(recipe);
-
-        /*
-        String[] grid2 = {" ", "1", " ", "1", "2", "1", " ", "1", " "};
-        ArrayList<String> keys2 = new ArrayList<>();
-        keys2.add("gold_ingot");
-        keys2.add("player_head");
-        Recipe recipe2 = new Recipe("crafting_shaped", grid2, keys2, "golden_apple", 1);
-        recipes.add(recipe2);
-        */
-
-        for (Recipe r : recipes) {
-            ArrayList<String> fileCommands = new ArrayList<>();
-            fileCommands.add(" {");
-            fileCommands.add("        \"type\": \"" + r.type + "\",");
-            fileCommands.add("        \"pattern\": [");
-            fileCommands.add("            \"" + r.grid[0] + r.grid[1] + r.grid[2] + "\",");
-            fileCommands.add("            \"" + r.grid[3] + r.grid[4] + r.grid[5] + "\",");
-            fileCommands.add("            \"" + r.grid[6] + r.grid[7] + r.grid[8] + "\"");
-            fileCommands.add("        ],");
-            fileCommands.add("        \"key\": {");
-            int counter = 1;
-            for (String key : r.keys) {
-                fileCommands.add("            \"" + counter + "\": {");
-                fileCommands.add("                \"item\": \"" + key + "\"");
-                if (r.keys.size() == counter) {
-                    fileCommands.add("            }");
-                } else {
-                    fileCommands.add("            },");
-                }
-                counter++;
-            }
-            fileCommands.add("        },");
-            fileCommands.add("        \"result\": {");
-            fileCommands.add("            \"item\": \"" + r.resultItem + "\",");
-            fileCommands.add("            \"count\": " + r.resultAmount);
-            fileCommands.add("        }");
-            fileCommands.add("    }");
-
-            FileData fileData = new FileData(r.resultItem + r.resultAmount, fileCommands, "recipe");
-            files.add(fileData);
-        }
     }
 
     private String callFunction(String functionName) {
