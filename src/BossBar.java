77cfe0943ba2c6424c8b5ddf092882bbecbb05f2
Wouter Baltus <w.baltus@resoftware.nl>
2022-05-06 16:33:42 +0200

    reworking the way the code is generated

---
+++ src/BossBar.java
@@ -0,0 +1,45 @@
+public class BossBar {
+
+    private String name;
+    private String prefix = "bossbar ";
+
+    public BossBar(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String remove() {
+        return prefix + "remove minecraft:" + name;
+    }
+
+    public String add(String title) {
+        return prefix + "add minecraft:" + name + " \"" + title + "\"";
+    }
+
+    public String setMax(int max) {
+        return prefix + "set minecraft:" + name + " max " + max;
+    }
+
+    public String setColor(String color) {
+        return prefix + "set minecraft:" + name + " color " + color;
+    }
+
+    public String setTitle(String title) {
+        return prefix + "set minecraft:" + name + " name \"" + title + "\"";
+    }
+
+    public String setVisible(Boolean visible) {
+        return prefix + "set minecraft:" + name + " visible " + visible;
+    }
+
+    public String setPlayers(String players) {
+        return prefix + "set minecraft:" + name + " players " + players;
+    }
+
+    public String setValue(String value) {
+        return prefix + "set minecraft:" + name + " value " + value;
+    }
+}
