71af072fb7c5ee9645a7349e89d5f137bd3679e6
PerfidyIsKey <myday.games@gmail.com>
2022-05-21 15:09:39 +0200

    add perks and added execute defaults

---
+++ src/Perk.java
@@ -0,0 +1,37 @@
+public class Perk {
+
+    private int id;
+    private String reward;
+    private String soundEffect;
+    private int activationTime;
+
+    private String rewardType;
+
+    public Perk(int id, String reward, String rewardType, String soundEffect, int activationTime) {
+        this.id = id;
+        this.reward = reward;
+        this.rewardType = rewardType;
+        this.soundEffect = soundEffect;
+        this.activationTime = activationTime;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+
+    public String getSoundEffect() {
+        return soundEffect;
+    }
+
+    public int getActivationTime() {
+        return activationTime;
+    }
+
+    public String getRewardType() {
+        return rewardType;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
