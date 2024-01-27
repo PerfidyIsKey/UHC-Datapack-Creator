2bc1f66be4afb014f98eba5ee2de3932bf545945
Bas Themen <bthemen4@gmail.com>
2024-01-27 02:35:37 +0100

    Cleanup of MATLAB team generator

--- MATLAB/Functions/getTeamScore.m
+++ MATLAB/Functions/getTeamScore.m
@@ -1,8 +1,8 @@
-function teamScore = getTeamScore(x,scores,teamNumber,teamSize,...
-    Players,ParticipantIndex,settings,mode)
+function teamScore = getTeamScore(x, scores, teamNumber, teamSize,...
+    mode)
 %% Initialization
 unitsNumber = length(x);            % Number of players
-teamScore = zeros(teamNumber,1);    % Preallocation
+teamScore   = zeros(teamNumber, 1); % Preallocation
 switch mode
     case 1  % Objective function
         power = 4;
@@ -12,25 +12,23 @@ end
 
 %% Determine team scores
 for i = 1:teamNumber
-    count = 0;  % Initialize counter
-    bestPlayer = 0;
+    count       = 0;  % Initialize counter
+    bestPlayer  = 0;
     for ii = 1:unitsNumber
         if x(ii) == i % If player is in this team, add their score
-            count = count + 1;  % Update counter
-%             scoreNoise = settings.noise/(Players(ParticipantIndex(ii)).Experience)^2*randn; % Add score noise
-%             playerScore = max(scores(ii)+scoreNoise,0);
-playerScore = scores(ii);
-            teamScore(i) = teamScore(i) + playerScore^power;   % Update team score
-            bestPlayer = max(bestPlayer,playerScore);
+            count           = count + 1;                        % Update counter
+            playerScore     = scores(ii);                       % Retrieve rank
+            teamScore(i)    = teamScore(i) + playerScore^power; % Update team score
+            bestPlayer      = max(bestPlayer, playerScore);     % Determine best rank in team
             if count > teamSize % If extra players are added, add a score of a pseudo-team mate
-                meanScore = max(bestPlayer,100)^power;
-                teamScore(i) = teamScore(i) + meanScore;
+                meanScore       = max(bestPlayer, 100)^power;
+                teamScore(i)    = teamScore(i) + meanScore;
             end
         end
     end
     
     if count < teamSize
-        teamScore(i) = teamScore(i) - (teamSize-count)*mean(scores)^power;
+        teamScore(i) = teamScore(i) - (teamSize - count)*mean(scores)^power;
     end
     
     if count > 0    % If the team has players assigned, scale the score
--- MATLAB/teamSelector_v2.m
+++ MATLAB/teamSelector_v2.m
@@ -1,111 +1,112 @@
 clear
 close all
 
-addpath('Functions','Documents','Data')
+addpath("Functions", "Documents", "Data")
 
 Players = struct;
-load('DataS50.mat')
+load("DataS51.mat")
 
 %% Input
 % Enter the players that are participating (corresponding numbers with
 % PlayerName variable in Players struct)
-ParticipantIndex = [1, 2, 17, 18, 25, 31, 48, 52];
+participantIndex = [1, 2, 17, 25, 44, 45, 48, 54, 56];
 
 %%% Enter the names of new players
-NewPlayers = ["mrminebase"];
-EstimatedRank = [20];
+newPlayers = ["Dan_Fingerman", "marckstef", "neokneipies", "blacksnake29", "BuildingBard300"];
+estimatedRank = [60, 20, 10, 20, 60];
 
 %%% Algorithm settings
-teamPlayer          = 3;        % Number of players per team
-rankLowerBound      = 5;        % Maximum negative deviation of score median
-rankUpperBound      = 10;        % Maximum positive deviation of score mean
-rankLowerTolerance	= rankLowerBound + 10;       % Maximum allowed negative deviation
-rankUpperTolerance  = rankUpperBound + 15;       % Maximum allowed positive deviation
-maxConnections      = 2;        % Maximum number of times players have played together
-scoreNoise          = 20;       % Additional score noise to account for inaccuracies
-plotResults         = true;    % Visualize results in real time
-verboseMode         = false;    % Allow messages
+teamPlayer          = 2;                    % Number of players per team
+rankLowerBound      = 5;                    % Maximum negative deviation of score median
+rankUpperBound      = 10;                   % Maximum positive deviation of score mean
+rankLowerTolerance	= rankLowerBound + 10;  % Maximum allowed negative deviation
+rankUpperTolerance  = rankUpperBound + 15;  % Maximum allowed positive deviation
+maxConnections      = 2;                    % Maximum number of times players have played together
+scoreNoise          = 20;                   % Additional score noise to account for inaccuracies
+plotResults         = true;                 % Visualize results in real time
+verboseMode         = false;                % Allow messages
 
 % Conversion
-settings = struct('players',teamPlayer,'rank',struct('LB',rankLowerBound,...
-    'UB',rankUpperBound,'LT',rankLowerTolerance,'UT',rankUpperTolerance),...
-    'connections',maxConnections,'noise',scoreNoise);
+settings = struct("players", teamPlayer, "rank", struct("LB", rankLowerBound, ...
+    "UB", rankUpperBound, "LT", rankLowerTolerance, "UT", rankUpperTolerance), ...
+    "connections", maxConnections, "noise", scoreNoise);
 
-NoN = length(NewPlayers);           % Number of new players
-NoP = length(Players) + NoN;        % Total number of players
-NoPar = length(ParticipantIndex);	% Number of participants
+newAmount           = length(newPlayers);           % Number of new players
+totalAmount         = length(Players) + newAmount;  % Total number of players
+participantAmount   = length(participantIndex);	    % Number of participants
 
 %% Participation criteria
 % Pull the players that are participating in this season
-playerName	= cell(NoPar+NoN,1);
-Ranks       = zeros(NoPar+NoN,1);
-for i = 1:NoPar + NoN
-    if i <= NoPar
-        playerName{i} = Players(ParticipantIndex(i)).PlayerName;
-        if ~isnan(Players(ParticipantIndex(i)).Rank)
-            Ranks(i) = Players(ParticipantIndex(i)).Rank;
+playerName	= strings(participantAmount + newAmount, 1);
+ranks       = zeros(participantAmount + newAmount, 1);
+for i = 1:participantAmount + newAmount
+    if i <= participantAmount
+        playerName(i) = Players(participantIndex(i)).PlayerName;
+        if ~isnan(Players(participantIndex(i)).Rank)
+            ranks(i) = Players(participantIndex(i)).Rank;
         else
-            lastActive = find(Players(ParticipantIndex(i)).Participation==1,1,'last');
-            Ranks(i) = Players(ParticipantIndex(i)).RankHistory(lastActive);
+            lastActive  = find(Players(participantIndex(i)).Participation == true, 1, "last");
+            ranks(i)    = Players(participantIndex(i)).RankHistory(lastActive);
         end
-        Players(ParticipantIndex(i)).Experience = sum(Players(ParticipantIndex(i)).Participation);
+        Players(participantIndex(i)).Experience = sum(Players(participantIndex(i)).Participation);
     else
-        playerName{i} = NewPlayers{i - NoPar};
-        Ranks(i) = EstimatedRank(i-NoPar);
-        ParticipantIndex(i) = NoP - NoN + i - NoPar;
-        Players(ParticipantIndex(i)).Experience = 1;
+        playerName(i)       = newPlayers(i - participantAmount);
+        ranks(i)            = estimatedRank(i - participantAmount);
+        participantIndex(i) = totalAmount - newAmount + i - participantAmount;
+        Players(participantIndex(i)).Experience = 1;
     end
 end
 
 %% Set up Genetic Algorithm
-players	= 1:length(Ranks);      % Label players
-scores	= Ranks;                % Label ranks
+players	= 1:length(ranks);      % Label players
+scores	= ranks;                % Label ranks
 
-teamSize        = settings.players;             % Number of players per team
-playersNumber	= length(players);              % Number of players
-teamNumber      = floor(playersNumber/teamSize);	% Number of teams
+teamSize        = settings.players;                 % Number of players per team
+playersNumber	= length(players);                  % Number of players
+teamNumber      = floor(playersNumber / teamSize);  % Number of teams
 
 %%% Specify bounds
-lb = ones(playersNumber,1);             % Lower bound (team 1)
-ub = teamNumber*ones(playersNumber,1);  % Upper bound (last team)
+lb = ones(playersNumber, 1);                % Lower bound (team 1)
+ub = teamNumber * ones(playersNumber, 1);   % Upper bound (last team)
 
 %%% Specify Genetic Algorithm options
-options = optimoptions('ga','Display','none', "OutputFcn", @(x, y, z) gaoutfun(x, y, z, playerName));
+options = optimoptions("ga", "Display", "none",...
+    "OutputFcn",  @(x,  y,  z) gaoutfun(x,  y,  z,  playerName));
 
 %% Evaluate Genetic Algorithm
 while true
-    for i = 1:length(Ranks)
-        scores(i) = max(Ranks(i)+settings.noise/(Players(ParticipantIndex(i)).Experience)^2*randn,0);
+    for i = 1:length(ranks)
+        noise       = settings.noise*exp(-Players(participantIndex(i)).Experience)*randn;   % Add random noise scaled with experience
+        scores(i)   = max(ranks(i) + noise,  0);
     end
     
     %%% Define functions
-    fun = @(x)groupPlayers(x,scores,teamNumber,teamSize,...
-        Players,ParticipantIndex,settings); % Objective function
-    nonlcon = @(x)constrainTeams(x,scores,teamNumber,teamSize,...
-        Players,ParticipantIndex,PlayerConnectivity,settings);  % Constraints
+    fun = @(x) groupPlayers(x, scores, teamNumber, teamSize);       % Objective function
+    nonlcon = @(x) constrainTeams(x, scores, teamNumber, teamSize, ...
+        Players, participantIndex, PlayerConnectivity, settings);   % Constraints
     
-    [x,fval,exitflag,output,population,popScores] = ga(fun,playersNumber,[],[],[],[],...
-        lb,ub,nonlcon,1:playersNumber,options);
+    [x, fval, exitflag, output, population, popScores] = ga(fun, playersNumber, [], [], [], [], ...
+        lb, ub, nonlcon, 1:playersNumber, options);
     
     %%% Retrieve constraints on exit
-    [g,h] = constrainTeams(x,scores,teamNumber,teamSize,...
-        Players,ParticipantIndex,PlayerConnectivity,settings);
+    [g, h] = constrainTeams(x, scores, teamNumber, teamSize, ...
+        Players, participantIndex, PlayerConnectivity, settings);
     
     %%% Check which constraints are violated
-    sizeBool    = isempty(find(g(1:2*teamNumber)>0,1));
-    topBool     = isempty(find(g((1:teamNumber)+2*teamNumber)>settings.rank.UT,1));
-    downBool	= isempty(find(g((1:teamNumber)+3*teamNumber)>settings.rank.LT,1));
+    sizeBool    = isempty(find(g(1:2*teamNumber) > 0, 1));
+    topBool     = isempty(find(g((1:teamNumber) + 2*teamNumber) > settings.rank.UT, 1));
+    downBool	= isempty(find(g((1:teamNumber) + 3*teamNumber) > settings.rank.LT, 1));
     
     if ~sizeBool
         % Team size violated
         if verboseMode == true
-            fprintf('Team sizes are not correct.\n')
+            fprintf("Team sizes are not correct.\n")
         end
     end
     if ~topBool
         % Upper tolerance violated --> increase upper tolerance
         if verboseMode == true
-            fprintf('Balance is too overpowered, increasing tolerance.\n')
+            fprintf("Balance is too overpowered,  increasing tolerance.\n")
         end
         if randi(2) == 1
             settings.rank.UB = settings.rank.UB + 1;
@@ -116,7 +117,7 @@ while true
     if ~downBool
         % Lower tolerance violated --> decrease lower tolerance
         if verboseMode == true
-            fprintf('Balance is too underwhelming, increasing tolerance.\n')
+            fprintf("Balance is too underwhelming,  increasing tolerance.\n")
         end
         if randi(2) == 1
             settings.rank.LB = settings.rank.LB + 1;
@@ -132,29 +133,18 @@ while true
 end
 
 %% Display results
-%%% Plot results
-if verboseMode == true
-    X = categorical(playerName);
-    X = reordercats(X,playerName);
-    figure();
-    bar(X,x)
-    ax = gca;
-    ax.XAxis.TickLabelInterpreter = 'none';
-end
-
 %%% Display results in command window
-teamScore = getTeamScore(x,scores,teamNumber,teamSize,...
-    Players,ParticipantIndex,settings,2);	% Get team scores
-meanScore = mean(scores);                   % Mean of team scores
+teamScore = getTeamScore(x, scores, teamNumber, teamSize, 2);   % Get team scores
+meanScore = mean(scores);                                       % Mean of team scores
 
-ColVec = ["Yellow";"Blue";"Red";"Purple";"Green";"Pink";"Black";"Orange";"Gray";...
-    "Aqua";"D.Red";"D.Blue";"D.Aqua";"D.Green";"D.Gray";"White"];   % Team colors
-fprintf('The mean score of all players is %3.0f.\n',meanScore)
+colVec = ["Yellow", "Blue", "Red", "Purple", "Green", "Pink", "Black", "Orange", "Gray",...
+    "Aqua", "D.Red", "D.Blue", "D.Aqua", "D.Green", "D.Gray", "White"];   % Team colors
+fprintf("The mean score of all players is %3.0f.\n", meanScore)
 
-printName	= strings(playersNumber,1); % Preallocation
+printName	= strings(playersNumber, 1); % Preallocation
 pCount      = 0;                        % Initialization
 for i = 1:teamNumber
-    names = strings(1,settings.players);    % Preallocation
+    names = strings(1, settings.players);   % Preallocation
     count = 0;                              % Initialization
     for ii = 1:playersNumber
         if x(ii) == i	% Add player if they are in this team
@@ -162,26 +152,26 @@ for i = 1:teamNumber
             count   = count + 1;
             pCount	= pCount + 1;
             
-            names(count)   = convertCharsToStrings(playerName{ii}); % Add player name to display list
+            names(count)   = convertCharsToStrings(playerName(ii)); % Add player name to display list
             if count == 1   % Update display operators
                 oper = '%s';
             else
-                oper((end+1):(end+4)) = ', %s';
+                oper((end + 1):(end + 4)) = ', %s';
             end
-            joinName            = ['team join Team',num2str(i-1),' ',playerName{ii}];
-            printName(pCount)	= convertCharsToStrings(joinName);  % Add player name to command list
+            printName(pCount)   = "team join Team" + num2str(i-1) + " " + playerName(ii);   % Add player name to command list
         end
     end
+
     % Convert scores to strings
-    score       = num2str(teamScore(i),'%3.0f');
-    devScore	= num2str(teamScore(i) - meanScore,'%+2.0f');
-    printChar = ['Team %7s: ',oper,' Team score: %s (%s)\n'];
-    names(names=="") = [];
-    fprintf(printChar,[ColVec{i},names,score,devScore])
+    score       = num2str(teamScore(i), "%3.0f");
+    devScore	= num2str(teamScore(i) - meanScore, "%+2.0f");
+    printChar = "Team %7s: " + oper + " Team score: %s (%s)\n";
+    names(names == "") = [];
+    fprintf(printChar, [colVec(i), names, score, devScore])
 end
 
 %% Write to file
-filename = fullfile('Documents','random_teams.mcfunction');   % Define document name
-fileID = fopen(filename,'w');           % Open document
-fprintf(fileID,'%s \n',printName);      % Rewrite document
-fclose(fileID);                         % Close document
+filename = fullfile("Documents", "random_teams.mcfunction");    % Define document name
+fileID = fopen(filename, "w");                                  % Open document
+fprintf(fileID, "%s \n", printName);                            % Rewrite document
+fclose(fileID);                                                 % Close document
