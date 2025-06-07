ef68b72b1542ec22feb00f43bd5c69a88dfd1300
Bas Themen <bthemen97@gmail.com>
2025-06-07 15:45:12 +0200

    Added main timer functionality to start_game

--- src/Enums/FileName.java
+++ src/Enums/FileName.java
@@ -16,9 +16,9 @@ public enum FileName {
     game_starter,
 
     // Timer functions
-    timer_tick_1,
-    timer_tick_5,
-    timer_tick_20,
+    timer_main_1,
+    timer_main_5,
+    timer_main_20,
     timer_control_point_5,
     timer_control_point_20,
 
