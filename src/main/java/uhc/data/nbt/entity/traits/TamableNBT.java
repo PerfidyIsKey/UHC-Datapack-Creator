e90d332e23a0f0351385a55a1614e03153c4e191
Bas Themen <bthemen97@gmail.com>
2025-12-20 01:48:02 +0100

    Added NBT for mobs that can be tamed by players

---
+++ src/main/java/uhc/data/nbt/entity/traits/TamableNBT.java
@@ -0,0 +1,53 @@
+package uhc.data.nbt.entity.traits;
+
+import uhc.data.nbt.tags.*;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * 🦴 **Tamable Trait**
+ * <p>
+ * Provides NBT support for mobs that can be owned by players,
+ * such as Wolves, Cats, and Parrots.
+ * </p>
+ */
+public interface TamableNBT<T extends TamableNBT<T>> {
+
+    /**
+     * Bridge method to access the entity's root {@link CompoundTag}.
+     */
+    CompoundTag root();
+
+    /**
+     * Sets the owner of the entity.
+     * <p>
+     * Note: In older Minecraft versions, this was stored as 'OwnerUUID' (String).
+     * In modern versions (1.16+), it is 'Owner' (IntArray).
+     * </p>
+     * @param uuid The UUID of the player who owns this mob.
+     */
+    @SuppressWarnings("unchecked")
+    default T owner(UUID uuid) {
+        Objects.requireNonNull(uuid, "Owner UUID cannot be null.");
+
+        long most = uuid.getMostSignificantBits();
+        long least = uuid.getLeastSignificantBits();
+
+        // Modern Minecraft uses the 4-int array format for 'Owner'
+        root().put(new IntArrayTag("Owner", new int[]{
+                (int) (most >> 32), (int) most,
+                (int) (least >> 32), (int) least
+        }));
+        return (T) this;
+    }
+
+    /**
+     * Sets whether the tamed animal is currently sitting.
+     * @param sitting True to force the animal to sit.
+     */
+    @SuppressWarnings("unchecked")
+    default T sitting(boolean sitting) {
+        root().put(new ByteTag("Sitting", (byte) (sitting ? 1 : 0)));
+        return (T) this;
+    }
+}
